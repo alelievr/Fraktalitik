0e92d032f7ed1f694c5fbd52d33ad85ca7f0e0ab
alelievr <alelievr@student.42.fr>
2018-05-02 18:37:20 +0200

    Added test sound particle systems

---
+++ Assets/Scenes/Alelievr/SoundParticles/AlelievrParticleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class AlelievrParticleController : MonoBehaviour
+{
+	public float	throttle { get; set; }
+	
+    ParticleSystem.EmissionModule		emission;
+    ParticleSystem.ShapeModule			shape;
+    ParticleSystem.NoiseModule			noise;
+    ParticleSystem.ExternalForcesModule	force;
+	
+	float								originalEmission;
+	float								originalStrength;
+
+	new ParticleSystem					particleSystem;
+
+	void Start ()
+	{
+		particleSystem = GetComponent< ParticleSystem >();
+
+		emission = particleSystem.emission;
+		originalEmission = emission.rateOverTime.constant;
+
+		shape = particleSystem.shape;
+		
+		noise = particleSystem.noise;
+		originalStrength = noise.strength.constant;
+
+		force = particleSystem.externalForces;
+	}
+	
+	void Update ()
+	{
+		if (throttle > .5f)
+			particleSystem.Emit((int)(throttle * 10));
+	/*	var strength = noise.strength;
+		var scrollSpeed = noise.scrollSpeed;
+
+		strength = throttle * originalStrength;
+		scrollSpeed = throttle;*/
+	}
+}
